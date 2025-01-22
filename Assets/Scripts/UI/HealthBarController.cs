5131b783b659881a55f0d4d9e934db6cd41583f3
张斌 <bin.zhang@appshahe.com>
2025-01-22 11:35:43 +0800

    feat：血条初步制作

---
+++ Assets/Scripts/UI/HealthBarController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class HealthBarController : MonoBehaviour
+{
+    [Header("Elements")]
+    public Transform healthBarTrans;
+    private UIDocument healthBarDoc;
+    private ProgressBar healthBar;
+
+    private void Awake()
+    {
+        healthBarDoc = GetComponent<UIDocument>();
+        healthBar = healthBarDoc.rootVisualElement.Q<ProgressBar>();
+        MoveToWorldPosition(healthBar, healthBarTrans.position, Vector2.zero);
+    }
+    private void MoveToWorldPosition(VisualElement element, Vector3 worldPos, Vector2 size)
+    {
+        Rect rect = RuntimePanelUtils.CameraTransformWorldToPanelRect(element.panel, worldPos, size, Camera.main);
+        element.transform.position = rect.position;
+    }
+    
+    [ContextMenu("Test")]
+    public void Test()
+    {
+        healthBarDoc = GetComponent<UIDocument>();
+        healthBar = healthBarDoc.rootVisualElement.Q<ProgressBar>();
+        MoveToWorldPosition(healthBar, healthBarTrans.position, Vector2.zero);
+    }
+}
